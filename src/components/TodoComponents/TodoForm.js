9bc597b6b536d4f795db964ab4af9e97b2e8b10f
Laura Daugherty <lauradaugherty6@gmail.com>
2019-05-29 15:31:41 -0700

    getting started

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,15 @@
+
+
+function TodoForm (props) {
+    render();
+        return (
+            <div>
+                <input>todo item</input>
+                <button>Add todo</button>
+                <button>Clear Completed</button>
+            </div>
+        )
+    }
+
+
+    
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,46 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from "react"
+import Todo from "./Todo";
+
+
+function TodoList(props) {
+    return (
+        <div>
+
+        <h1>This is my list</h1>
+        <div>
+            {
+                props.Todo.map(Todo => <h1>{todosArray.task}</h1>)
+            }
+        </div>
+
+        </div>
+        
+    )
+}
+
+export default TodoList;
+
+export const todosArray = [
+    {
+      task: "Organize Garage",
+      id: 1528817077286,
+      completed: false
+    },
+    {
+      task: "Bake Cookies",
+      id: 1528817084358,
+      completed: false
+    },
+    {
+      task: "DO THE THING",
+      id: 15288170,
+      completed: false
+    },
+    {
+      task: "Do the other thing",
+      id: 152858,
+      completed: false
+    }
+  ];
